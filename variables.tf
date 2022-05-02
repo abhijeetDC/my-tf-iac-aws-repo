0e5304556f09051a644ccd14919575a360b16e79
abhijeetDC <96890313+abhijeetDC@users.noreply.github.com>
2022-05-02 19:36:50 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,10 +13,7 @@ variable "instance_type" {
   default     =  "t2.micro"
 }
 
-variable "security_group" {
-  description = "Name of security group"
-  default     = "my-jenkins-security-group"
-}
+
 
 variable "tag_name" {
   description = "Tag Name of for Ec2 instance"
